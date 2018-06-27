32fac8d783e7b62d39b1e29fb1dda2bf20d7b95d
Márcio Rodrigo <39975073+mrodrigochaves@users.noreply.github.com>
2018-06-27 05:48:12 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -78,7 +78,9 @@ this.x = x;
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
         const allEnemies = [
-                new Enemy(0, 160, 20)
+                new Enemy(0, 160, 20),
+                new Enemy(404, 60, 255),
+                new Enemy(204, 265, 150)
         ];
         const player = new Player();
 // This listens for key presses and sends the keys to your
